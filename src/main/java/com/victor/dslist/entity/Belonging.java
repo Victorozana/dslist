5e2c0bebd5cd03ba7599f659d00bc03adac1122d
victor <victor.ozprofissional@gmail.com>
2025-08-19 10:00:10 -0300

    game_list, belonging, domain model

---
+++ src/main/java/com/victor/dslist/entity/Belonging.java
@@ -0,0 +1,54 @@
+package com.victor.dslist.entity;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_beloging")
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+
+    private Integer position;
+
+    public Belonging() {
+    }
+
+    public Belonging(Integer position, Game game, GameList gameList) {
+        this.position = position;
+        id.setGame(game);
+        id.setGameList(gameList);
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Belonging belonging = (Belonging) o;
+        return Objects.equals(id, belonging.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
